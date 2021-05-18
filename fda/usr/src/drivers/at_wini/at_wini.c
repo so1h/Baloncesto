bdf7715fd393ca60350c8e8b874973b4e901d746
Pedro Pablo Lopez Rodriguez <pedropablo.lopez@upm.es>
2021-05-18 12:17:41 +0200

    Se ha añadido el clock counter al final de la traza opIDE tanto en la parte de Minix como en la de Windows. En Windows está pendiente convertir el valor del contador en microsegundos. Realmente esto se hizo en el commit anterior.

--- fda/usr/src/drivers/at_wini/at_wini.c
+++ fda/usr/src/drivers/at_wini/at_wini.c
@@ -1129,7 +1129,7 @@ PRIVATE int do_transfer(struct wini *wn, unsigned int precomp,
 	*((unsigned int *)&trazaIDE[ 5]) = sector ;                        /* PP */
 	*((unsigned int *)&trazaIDE[ 9]) = count ;                         /* PP */
 	*((unsigned int *)&trazaIDE[13]) = do_dma ;                        /* PP */
-    u9_raw_cc((char *)&trazaIDE, 17) ;                                         /* PP */ 
+    u9_raw_cc((char *)&trazaIDE, 17) ;                                          /* PP */ 
   }                                                                    /* PP */
 #endif
 
